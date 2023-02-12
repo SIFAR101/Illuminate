4b8c0bdfdd1a019399b8aef477e3b51e375029c5
Simona Barone <simona_barone@hotmail.com>
2023-02-12 14:25:32 +0100

    fixed small style issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,14 @@ class User < ApplicationRecord
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, omniauth_providers: %i[google_oauth2 facebook]
 
-         def self.from_omniauth(auth)
-          user = User.where(email: auth.info.email).first
-          if user
-            user.update(provider: auth.provider, uid: auth.uid)
-          else
-            user = User.create(
-              email: auth.info.email,
-              password: Devise.friendly_token[0,20],
-              full_name: auth.info.name,
-              provider: auth.provider,
-              uid: auth.uid
-            )
-          end
-          user
-        end
-      end
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+      user.update(provider: auth.provider, uid: auth.uid)
+    else
+      user = User.create(email: auth.info.email, password: Devise.friendly_token[0, 20],
+                         full_name: auth.info.name, provider: auth.provider, uid: auth.uid)
+    end
+    user
+  end
+end
