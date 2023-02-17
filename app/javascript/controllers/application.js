bb0ec3475945fb3474c3e2a74850146e9a27b9a0
Francis Servignat <francis.servignat@outlook.com>
2023-02-17 18:05:19 +0100

    feat: fade-in scale animate and bounce animations on text and buttons

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,40 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+anime({
+  targets: document.querySelector('.headline'),
+  opacity: [0, 1],
+  translateY: [20, 0],
+  easing: 'easeOutExpo',
+  duration: 2000,
+  delay: 500
+});
+
+anime({
+  targets: document.querySelector('.learn-btn'),
+  opacity: [0, 1],
+  scale: [0.5, 1],
+  easing: 'easeOutExpo',
+  duration: 1500,
+  delay: 1000
+});
+
+anime({
+  targets: document.querySelectorAll('.paragraph-intro'),
+  opacity: [0, 1],
+  translateX: [-30, 0],
+  easing: 'easeOutExpo',
+  duration: 1000,
+  delay: anime.stagger(500, {start: 1000})
+});
+
+anime({
+  targets: document.querySelector('.download-btn'),
+  scale: [1, 1.2],
+  duration: 1000,
+  direction: 'alternate',
+  loop: true,
+  easing: 'linear',
+});
