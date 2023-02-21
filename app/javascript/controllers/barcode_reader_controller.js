922fa133324655f472a3884b7e8e977459088558
Dari Goldman <darigoldman@gmail.com>
2023-02-21 16:37:30 -0500

    fix: bringing back elements of Simona's original code for troubleshooting. Changed some styling to match Figma

--- app/javascript/controllers/barcode_reader_controller.js
+++ app/javascript/controllers/barcode_reader_controller.js
@@ -3,6 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="barcode-reader"
 export default class extends Controller {
   connect() {
+    const header = document.querySelector('h1')
+    console.log('Hello there.')
     window.addEventListener('load', function () {
       let selectedDeviceId;
       const codeReader = new ZXing.BrowserMultiFormatReader()
@@ -11,9 +13,24 @@ export default class extends Controller {
         .then((videoInputDevices) => {
           const sourceSelect = document.getElementById('sourceSelect')
           selectedDeviceId = videoInputDevices[0].deviceId
+          if (videoInputDevices.length >= 1) {
+            videoInputDevices.forEach((element) => {
+              const sourceOption = document.createElement('option')
+              sourceOption.text = element.label
+              sourceOption.value = element.deviceId
+              sourceSelect.appendChild(sourceOption)
+            })
+
+            sourceSelect.onchange = () => {
+              selectedDeviceId = sourceSelect.value;
+            };
+
+            const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+            sourceSelectPanel.style.display = 'block'
+          }
 
           // Removed "Start" link. When the video div is clicked, scan begins.
-          document.getElementById('video').addEventListener('click', () => {
+          document.getElementsByClassName('video-container')[0].addEventListener('click', () => {
             codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
               if (result) {
                 console.log(result)
@@ -23,6 +40,9 @@ export default class extends Controller {
               if (err && !(err instanceof ZXing.NotFoundException)) {
                 console.error(err)
                 document.getElementById('result').textContent = err
+                codeReader.reset()
+                document.getElementById('result').textContent = '';
+                console.log('Reset.')
               }
             })
             console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
