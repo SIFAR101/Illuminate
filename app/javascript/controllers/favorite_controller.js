aa46dc6d794433490f7755838d5203ee06ac5e4e
Dari Goldman <darigoldman@gmail.com>
2023-03-13 13:59:09 +0100

    fix: added code in comments to connect favorites controller if using

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -1,18 +1,30 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "heart" ]
-  static values = {
-    product: Object,
-    user: Object
-  }
-  connect(){
-    console.log("Favorite Controller version 1 connected.")
-    this.addToFavorites()
+  static targets = [ "heart", "text" ]
 
-  }
-  addToFavorites(event){
-    event.preventDefault()
-    console.log(event)
+  toggle() {
+    const heartIcon = this.heartTarget
+    const text = this.textTarget
+    const productId = this.element.dataset.productId
+
+    heartIcon.classList.toggle('fa-regular')
+    heartIcon.classList.toggle('fa-solid')
+    text.textContent = heartIcon.classList.contains('fa-solid') ? "This product is in your favorites!" : "Add to your favorites"
   }
 }
+// export default class extends Controller {
+//   static targets = [ "heart" ]
+//   static values = {
+//     product: Object,
+//     user: Object
+//   }
+//   connect(){
+//     console.log("Favorite Controller version 1 connected.")
+//     this.addToFavorites()
+
+//   }
+//   addToFavorites(event){
+//     console.log(event)
+//   }
+// }
